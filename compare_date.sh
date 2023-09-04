1aaf43464d5736a26a3efd1885408de8a9fb8025
Andrew Brantley <github@andbrant.com>
2023-09-04 18:07:46 -0500

    getting auto updates ready

--- compare_date.sh
+++ compare_date.sh
@@ -1,4 +1,84 @@
-remote_date=$(curl -k https://hot.mini.lan/nebula/drew-lin-lap.date 2>/dev/null )
-local_date=$(date -r /etc/nebula/ca.crt "+%s")
+# this needs to be run as root for non-phones... like not with sudo, but change to the root user... for some reason 
+# ie sudo su -c './compare_date.sh'
 
-test "$remote_date" > "$local_date" && echo true || echo false
+# https://hot.mini.lan/nebula/pixel6.date
+# https://hot.mini.lan/nebula/pixel6.pass.enc
+# https://hot.mini.lan/nebula/pixel6.tar.enc
+
+if [ -e '.env' ]; then
+    source .env;
+fi
+
+if [ -z "$BASE_URI" ]; then
+    echo "\$BASE_URI is not defined; Exiting";
+    exit -1;
+fi
+if [ -z "$DEVICE_NAME" ]; then
+    echo "\$DEVICE_NAME is not defined; Exiting";
+    exit -1;
+fi
+if [ -z "$TEST_FILE_PATH" ]; then
+    echo "\$TEST_FILE_PATH is not defined; Exiting";
+    exit -1;
+fi
+
+if [ -z "$NOT_ROOT" ]; then
+    export NOT_ROOT='false';
+fi
+
+if [ "$NOT_ROOT" = 'false' ]; then
+    if [ -z "$var_dir" ]; then
+        echo "var_dir is not defined; exiting"
+        exit -1;
+    fi
+fi
+
+if [ "true" = "$NOT_ROOT" ]; then
+  export tar_location="$PWD";
+else
+  export tar_location="$var_dir/tar_stuff"; # this will fail if the tar dir does not exsist 
+fi
+
+download(){
+    curl -k -o "$tar_location/out.pass.enc" "$BASE_URI/$DEVICE_NAME.pass.enc"
+    curl -k -o "$tar_location/out.tar.enc" "$BASE_URI/$DEVICE_NAME.tar.enc"
+}
+
+restartNebula(){
+    echo 'restarting nebula';
+    systemctl restart nebula && echo 'nebula restarted' || echo 'nebula could not restart'
+}
+
+process(){
+    ./process_tar.sh 
+}
+
+takeActions(){
+    download &&
+    process && 
+    restartNebula # TODO make sure we're not a phone
+}
+
+if [ -e "$TEST_FILE_PATH" ]; then
+
+    remote_date=$(curl -k "$BASE_URI/$DEVICE_NAME.date" 2>/dev/null )
+    local_date=$(date -r "$TEST_FILE_PATH" "+%s")
+    #            date -r  out.tar.enc       +%s
+
+
+    # test "$remote_date" > "$local_date" && echo true || echo false
+
+    # if test file is not found
+    # if test file is old 
+
+    if [ "$remote_date" -gt "$local_date" ]; then
+        echo 'New version found';
+        takeActions
+    else
+        echo 'No new version found';
+    fi
+
+else
+    echo 'old file missing... downloading';
+    takeActions
+fi
--- process_tar.sh
+++ process_tar.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -z "$NOT_ROOT" ]; then
+    export NOT_ROOT='false';
+fi
 
 # vars from ansible 
 if [ -z "$nebula_config_client_folder" ]; then
@@ -8,28 +11,27 @@ if [ -z "$var_dir" ]; then
   var_dir="/var/yeltnar-nebula";
 fi
 
-SUDO_USER_HOME=$(su $SUDO_USER -c 'echo $HOME')
+if [ -z "$SUDO_USER" ]; then # not running as root, use current info 
+  BASHRC_HOME="$HOME";
+  REAL_USER="$USER"
+else
+  REAL_USER=$SUDO_USER
+  BASHRC_HOME=$(su $REAL_USER -c 'echo $HOME')
+fi
 
 localDecrypt(){
-  export PRIVKEY=$var_dir/id_rsa          
-  export AES_KEY_ENC=$var_dir/tar_stuff/out.pass.enc;
-  export ENCRYPTED_FILE=$var_dir/tar_stuff/out.tar.enc;
-  export DECRYPTED_FILE=$var_dir/tar_stuff/out.tar; 
-  export workdir="$var_dir/workdir"
 
   mkdir -p "$workdir" # this will fail if var_dir is not there 
 
-  $SUDO_USER_HOME/playin/custom_bashrc/bin/rsa_enc decrypt
+  $BASHRC_HOME/playin/custom_bashrc/bin/rsa_enc decrypt
 }
-localDecrypt
 
 extractContent(){
-  cd $var_dir/tar_stuff/
-  $SUDO_USER_HOME/playin/custom_bashrc/bin/extract out.tar
+  cd "$tar_location"
+  $BASHRC_HOME/playin/custom_bashrc/bin/extract out.tar
 
-  chown "$SUDO_USER" *
+  chown "$REAL_USER" *
 }
-extractContent
 
 moveFiles(){
   #  TODO ignore errors when copying 
@@ -38,7 +40,6 @@ moveFiles(){
 
   # need root
   declare -a str_arr=("host.crt" "host.key" "config.yml")
-
   for val in ${str_arr[@]}; do
       cp "$var_dir/tar_stuff/$val" "$nebula_config_client_folder/$val"
   done
@@ -46,10 +47,10 @@ moveFiles(){
   # need root 
   cp "$var_dir/tar_stuff/ca.crt" "$nebula_config_client_folder/inputfiles/ansible.ca.crt.new"
 }
-moveFiles
 
 joinCaCrts(){
-  # TODO need root 
+  # TODO there are some rotten vars here 
+  # need root 
   cd "$nebula_config_client_folder"
 
   out_file="$nebula_config_client_folder/ca.crt"
@@ -58,5 +59,44 @@ joinCaCrts(){
 
   ls inputfiles | awk '/ca.crt/{print "cat inputfiles/" $1 " >> ca.crt"}' | bash
 }
-joinCaCrts
 
+# checks if provided function is in the lit of functions
+if [ "true" = "$NOT_ROOT" ]; then
+
+  if [ -z "$PRIVKEY" ]; then
+    export PRIVKEY=$PWD/id_rsa
+  fi
+  if [ -z "$AES_KEY_ENC" ]; then
+    export AES_KEY_ENC=$PWD/out.pass.enc;
+  fi
+  if [ -z "$ENCRYPTED_FILE" ]; then
+    export ENCRYPTED_FILE=$PWD/out.tar.enc;
+  fi
+  if [ -z "$DECRYPTED_FILE" ]; then
+    export DECRYPTED_FILE=$PWD/out.tar; 
+  fi
+  if [ -z "$workdir" ]; then
+    export workdir="$PWD"
+  fi
+  if [ -z "$tar_location" ]; then
+    export tar_location="$PWD";
+  fi
+
+  localDecrypt
+  extractContent
+else 
+  export PRIVKEY=$var_dir/id_rsa          
+  export AES_KEY_ENC=$var_dir/tar_stuff/out.pass.enc;
+  export ENCRYPTED_FILE=$var_dir/tar_stuff/out.tar.enc;
+  export DECRYPTED_FILE=$var_dir/tar_stuff/out.tar; 
+  export workdir="$var_dir/workdir";
+
+  if [ -z "$tar_location" ]; then
+    export tar_location="$var_dir/tar_stuff";
+  fi
+
+  localDecrypt
+  extractContent
+  moveFiles
+  joinCaCrts
+fi
